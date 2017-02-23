2e1c39f024d200de40edec4a8dc0f4bb94c39f4d
cami7ord <cami7ord@gmail.com>
2017-02-23 18:50:26 -0500

    Job converting the file using LAME library and modifying the info of the voices after the conversion to set converted true and the destination_url

--- app/jobs/convert_voices_job.rb
+++ app/jobs/convert_voices_job.rb
@@ -3,7 +3,14 @@ class ConvertVoicesJob < ApplicationJob
 
   def perform(*params)
 	Voice.where(converted: false).find_each do |voice|
-	  puts "Convert the voice: #{voice.name}"
+		source_path = Rails.root.to_s + "/public" + voice.source_url.to_s
+		puts "Convert the voice: #{source_path}"
+		destination_path = File.dirname(source_path) + "/out.mp3"
+	    puts "To voice: #{destination_path}"
+	    system "lame #{source_path} #{destination_path}"
+	    voice.destination_url = destination_path
+	    voice.converted = true
+	    voice.save!
 	end
   end
 end
